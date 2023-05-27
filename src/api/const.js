34e2e793348a672bc92a9dfcfbbae4847107fddd
nelllyai <worldpiece000@gmail.com>
2023-05-27 15:31:30 +0300

    add wss protocol

--- src/api/const.js
+++ src/api/const.js
@@ -1,2 +1,2 @@
 export const URL_API = 'https://scarce-brass-visitor.glitch.me';
-export const WEBSOCKET_URL_API = 'ws://scarce-brass-visitor.glitch.me';
+export const WEBSOCKET_URL_API = 'wss://scarce-brass-visitor.glitch.me';
